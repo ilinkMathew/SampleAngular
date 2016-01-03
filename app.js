4bd2c2d1901c0b3e5fa34a739a84495e1708da19
=Nikil Mathew <ilinkmathew@gmail.com>
2016-01-04 01:01:01 +0530

    inital Angular material convert

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-var app = angular.module('BlankApp', ['ngMaterial'])
+var app = angular.module('ServiceApp', ['ngMaterial','ngRoute'])
 .config(function($mdThemingProvider){
       $mdThemingProvider.theme('default')
-    .primaryPalette('pink')
-    .accentPalette('orange');
+    .primaryPalette('blue')
+    .accentPalette('red')
+    .backgroundPalette('blue-grey'); 
 })
+.config(['$routeProvider',function($routeProvider){
+    $routeProvider.when('/Create',{
+        templateUrl:'templates/Create.html',
+        controller:'createController'
+    
+    }).
+    when('/Fix',{
+        templateUrl:'templates/Fix.html'
+    })
+    .when('/LookUp',{
+        templateUrl:'templates/LookUp.html'
+    })
+    .otherwise({
+       redirectTo:'/' 
+    });
+
+}])
+
+.controller('createController',['$scope',function($scope){
+ console.log("create ctrl");
+    $scope.homePageView = false;
+    $scope.createView = function(){
+    $scope.homePageView = false;
+    console.log("create view:"+$scope.homePageView);
+};    return $scope.homePageView;
+//$scope.createView();
+}])
+.controller('MainController',['$scope',function($scope){
+   
+    //$scope.homePageView = false;
+    console.log($scope.homePgeView);
+}]);    
